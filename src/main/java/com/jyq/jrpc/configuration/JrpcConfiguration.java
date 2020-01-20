1b67a4e0b9600db2a27f7e075c8d20e529f6bbc1
hz-jyq <864971290@qq.com>
2020-01-20 10:16:26 +0800

    Update JrpcConfiguration.java

--- src/main/java/com/jyq/jrpc/configuration/JrpcConfiguration.java
+++ src/main/java/com/jyq/jrpc/configuration/JrpcConfiguration.java
@@ -5,8 +5,6 @@ package com.jyq.jrpc.configuration;
 
 /**
  *
- * @author wb-jyq573332
- * @version $Id: JrpcConfiguration.java, v 0.1 2020年01月19日 16:08 wb-jyq573332 Exp $
  */
 public class JrpcConfiguration {
 }
